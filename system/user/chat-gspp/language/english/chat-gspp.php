9c4bf4e8a63019eca315ae926c1466f9e1aa320b
Eric Wilson <wilsones1991@gmail.com>
2025-02-07 11:22:15 -0800

    Remove language directory

--- system/user/chat-gspp/language/english/chat-gspp.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$lang = [
-    'gspp_charts_module_name'        => 'Chat GSPP',
-    'gspp_charts_module_description' => 'Generates a chat interface for PrivateGPT instance.',
-    'gspp_charts_settings'           => 'Chat GSPP Settings',
-    ''                            => '',
-];
