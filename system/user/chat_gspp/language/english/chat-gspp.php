6010c0a8377442b370f2ad653a1badc5a35ed19a
Eric Wilson <wilsones1991@gmail.com>
2025-02-07 11:35:20 -0800

    Add language files

---
+++ system/user/chat_gspp/language/english/chat-gspp.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang = [
+    'gspp_charts_module_name'        => 'Chat GSPP',
+    'gspp_charts_module_description' => 'Generates a chat interface for PrivateGPT instance.',
+    'gspp_charts_settings'           => 'Chat GSPP Settings',
+    ''                            => '',
+];
